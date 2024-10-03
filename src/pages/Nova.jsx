c0ba97cbfe4ff54023545933647994b3ef9e51b6
GUSTAVO LEONEL SALVADOR <202402875361@alunos.ibmec.edu.br>
2024-10-03 17:04:06 -0300

    03-10

---
+++ src/pages/Nova.jsx
@@ -0,0 +1,11 @@
+import NovoComponente from "../components/NovoComponente/NovoComponente";
+
+const Nova = () => {
+    return (<>
+                <h1>Título da Página</h1>
+                <p>Hello World</p>
+                < NovoComponente />
+            </>);
+} 
+    
+export default Nova; 
